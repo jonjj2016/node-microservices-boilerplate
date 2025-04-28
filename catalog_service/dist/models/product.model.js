5aaecaafd80bb4d53f97b4d28286c7b2bbe41745
Jon <jonmartirosyan1988@gmail.com>
2025-04-29 01:49:59 +0800

    initial commit

---
+++ catalog_service/dist/models/product.model.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(name, price, stock, description, id) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+        this.id = id;
+    }
+}
+exports.Product = Product;
