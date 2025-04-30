3356bae92100e1bc86c5b3022f740143e9d220ea
Jon <jonmartirosyan1988@gmail.com>
2025-04-30 17:18:43 +0800

    fix: fixet typing issues and package

--- catalog_service/dist/repository/__test__.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=__test__.js.map
--- catalog_service/src/express.app.ts
+++ catalog_service/src/express.app.ts
@@ -4,6 +4,6 @@ import { catalogRoutes } from './api/catalog.routes.js'
 export const app = express()
 app.use(express.json())
 
-app.use('/api/v1/product', catalogRoutes)
+app.use('/api/v1/product/bb', catalogRoutes)
 
 export default app
