59d8a1eafa8cc03d24a5899076d3fd71ae234e26
Jon <jonmartirosyan1988@gmail.com>
2025-05-02 13:27:07 +0800

    added routes to order service

--- order_service/src/express-app.ts
+++ order_service/src/express-app.ts
@@ -1,6 +1,14 @@
 import express from 'express'
 
+import cors from 'cors'
+import { cartRoutes } from './routes/cart.routes'
+import { orderRoutes } from './routes/order.routes'
+
 export const app = express()
+
+app.use(cors())
 app.use(express.json())
 
+app.use('/carts', cartRoutes)
+app.use('/orders', orderRoutes)
 export default app
---
+++ order_service/src/routes/cart.routes.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+const router = express.Router()
+
+router.get('/', (request, response) => {
+  response.status(200).json({ message: 'Hello there Request:GET' })
+})
+
+router.post('/', (request, response) => {
+  response.status(200).json({ message: 'Hello there Request:POST' })
+})
+
+router.patch('/:id', (request, response) => {
+  response.status(200).json({ message: 'Hello there Request:PATCH' })
+})
+
+router.get('/:id', (request, response) => {
+  response.status(200).json({ message: 'Hello there Request:GET/:id' })
+})
+
+router.delete('/:id', (request, response) => {
+  response.status(200).json({ message: 'Hello there Request:DELETE' })
+})
+
+export { router as cartRoutes }
